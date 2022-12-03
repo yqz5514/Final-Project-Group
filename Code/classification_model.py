0c319ef6bba320c000e25d81a2e5de2e13b63070
angeolson <78054238+angeolson@users.noreply.github.com>
2022-12-03 11:32:16 -0500

    Unzip file, get .csv

--- Code/classification_model.py
+++ Code/classification_model.py
@@ -2,5 +2,5 @@
 import os
 
 # step 1: load data from online
-os.system('wget https://www.kaggle.com/datasets/crowdflower/twitter-airline-sentiment/download?datasetVersionNumber=4')
+os.system('unzip archive(4).zip')
 
