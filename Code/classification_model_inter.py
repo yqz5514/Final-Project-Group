6b09c179c17f174f4e2f7992857d708834cb7c24
angeolson <78054238+angeolson@users.noreply.github.com>
2022-12-04 10:33:43 -0500

    Update classification_model_inter.py

--- Code/classification_model_inter.py
+++ Code/classification_model_inter.py
@@ -20,7 +20,7 @@ torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
 OUTPUT_DIM = 3
 BATCH_SIZE = 64
-MAX_VOCAB_SIZE = 25_000
+#MAX_VOCAB_SIZE = 25_000
 MAX_LEN = 300
 N_EPOCHS = 10
 LR = 0.001
@@ -68,7 +68,7 @@ def create_data_loader(df, tokenizer, max_len=MAX_LEN, batch_size=BATCH_SIZE):
         tokenizer = tokenizer,
         max_len = max_len,
         input_col = input_col)
-    return DataLoader(ds, batch_size=BATCH_SIZE, collate_fn=data_collator)
+    return DataLoader(ds, batch_size=BATCH_SIZE, collate_fn=data_collator, drop_last=True)
 
 
 # %% -------------------------------------- Dataset Class ------------------------------------------------------------------
@@ -107,23 +107,43 @@ class nlpDataset(Dataset):
 
 
 # %% -------------------------------------- Model Class ------------------------------------------------------------------
-class BERT_PLUS_MLP(nn.Module):
+class BERT_PLUS_RNN(nn.Module):
 
-    def __init__(self, bert, num_class, hidden_dim):
-        super(BERT_PLUS_MLP, self).__init__()
+    def __init__(self, bert, no_layers, hidden_dim, output_dim, batch_size):
+        super(BERT_PLUS_RNN, self).__init__()
         self.bert = bert
-        self.fc1 = nn.Linear(bert.pooler.dense.out_features, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, num_class)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.2)
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.embedding_dim = bert.config.to_dict()['hidden_size']
+        self.no_layers = no_layers
+        self.lstm = nn.LSTM(input_size=self.embedding_dim, hidden_size=self.hidden_dim, num_layers=no_layers,
+                            batch_first=True)
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(self.hidden_dim, self.output_dim)
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, input_ids, attention_mask):
-        _, cls_hs = self.bert(input_ids=input_ids, attention_mask=attention_mask, return_dict=False)
-        x = self.fc1(cls_hs)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+    def forward(self, x, hidden, attention_mask):
+        batch_size = x.size(0)
+        embedded = self.bert(input_ids=x,attention_mask=attention_mask)[0]
+        input = embedded.permute(1, 0, 2)
+        #lstm_out, hidden = self.lstm(input, hidden)
+        lstm_out, hidden = self.lstm(embedded, hidden)
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        #out = self.fc(lstm_out)
+        out = self.softmax(out)
+        out = out.view(batch_size, -1, self.output_dim)
+        out = out[:, -1]
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+        c0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+        hidden = (h0, c0)
+        return hidden
+
 
 # %% -------------------------------------- Data Prep ------------------------------------------------------------------
 # step 1: load data from .csv
@@ -156,70 +176,69 @@ test_loader = create_data_loader(test, tokenizer=tokenizer, max_len=MAX_LEN, bat
 
 # %% -------------------------------------- Model ------------------------------------------------------------------
 bert = AutoModel.from_pretrained(checkpoint)
-model = BERT_PLUS_MLP(bert, OUTPUT_DIM, 500)
-
-
-optimizer = AdamW(model.parameters(), lr=LR)
-num_training_steps = N_EPOCHS * len(train_loader)
-lr_scheduler = get_scheduler( "linear", optimizer=optimizer,
-                              num_warmup_steps=0, num_training_steps=num_training_steps)
-print(num_training_steps)
-
+#freeze the pretrained layers
+for param in bert.parameters():
+    param.requires_grad = False
+no_layers = 3
+hidden_dim = 256
+clip = 5
+model = BERT_PLUS_RNN(bert, no_layers, hidden_dim, OUTPUT_DIM, BATCH_SIZE)
 model.to(device)
 
+optimizer = AdamW(model.parameters(), lr=LR)
+criterion = torch.nn.BCELoss()
+
 # Store our loss and accuracy for plotting
-train_loss_set = []
 
 valid_loss_min = np.Inf
 epoch_tr_loss, epoch_vl_loss = [], []
 epoch_tr_acc, epoch_vl_acc = [], []
+
+
 
 for epoch in range(N_EPOCHS):
     # Tracking variables
     train_losses = []
     train_acc = []
     model.train()
+    h = model.init_hidden(BATCH_SIZE)
     for batch in train_loader:
-        # clear previously calculated gradients
+        inputs, labels, attention_mask = batch['input_ids'].to(device), batch['labels'].to(device), batch['attention_mask'].to(device)
+        h = tuple([each.data for each in h])
         model.zero_grad()
-        outputs = model(input_ids= batch['input_ids'].to(device), attention_mask=batch['attention_mask'].to(device))
-        criterion = torch.nn.BCEWithLogitsLoss()
-        loss = criterion(outputs.view(-1, OUTPUT_DIM), batch['labels'].type_as(outputs).view(-1, OUTPUT_DIM))
+        output, h = model(inputs, h, attention_mask)
+        loss = criterion(output, labels.float())
         loss.backward()
         train_losses.append(loss.item())
-        optimizer.step()
-        lr_scheduler.step()
-        optimizer.zero_grad()
         # Update tracking variables
-        THRESHOLD = 0.5
-        preds = outputs.detach().cpu().numpy()
-        preds[preds >= THRESHOLD] = 1
-        preds[preds < THRESHOLD] = 0
-        # accuracy = accuracy_score(y_true=batch['labels'].detach().cpu().numpy().astype(int),
-        #                           y_pred=(np.rint(outputs.detach().cpu().numpy())).astype(int))
-        accuracy = accuracy_score(y_true=batch['labels'].detach().cpu().numpy().astype(int),
-                                  y_pred=preds.astype(int))
+        preds = output.detach().cpu().numpy()
+        new_preds = np.zeros(preds.shape)
+        for i in range(len(preds)):
+            new_preds[i][np.argmax(preds[i])] = 1
+        accuracy = accuracy_score(y_true=batch['labels'].cpu().numpy().astype(int),
+                                  y_pred=new_preds.astype(int))
         train_acc.append(accuracy)
+        nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
 
     val_losses = []
     val_acc = []
     model.eval()
+    val_h = model.init_hidden(BATCH_SIZE)
     for batch in valid_loader:
-        with torch.no_grad():
-            outputs = model(input_ids=batch['input_ids'].to(device), attention_mask=batch['attention_mask'].to(device))
-            criterion = torch.nn.BCEWithLogitsLoss()
-            loss = criterion(outputs.view(-1, OUTPUT_DIM), batch['labels'].type_as(outputs).view(-1, OUTPUT_DIM))
-            val_losses.append(loss.item())
-            # Update tracking variables
-            THRESHOLD = 0.5
-            preds = outputs.detach().cpu().numpy()
-            preds[preds >= THRESHOLD] = 1
-            preds[preds < THRESHOLD] = 0
-            # accuracy = accuracy_score(y_true=batch['labels'].detach().cpu().numpy().astype(int),
-            #                           y_pred=(np.rint(outputs.detach().cpu().numpy())).astype(int))
-            val_accuracy = accuracy_score(y_true=batch['labels'].detach().cpu().numpy().astype(int),
-                                      y_pred=preds.astype(int))
-            val_acc.append(val_accuracy)
+        inputs, labels, attention_mask = batch['input_ids'].to(device), batch['labels'].to(device), batch['attention_mask'].to(device)
+        val_h = tuple([each.data for each in val_h])
+        output, val_h = model(inputs, val_h, attention_mask)
+        val_loss = criterion(output, labels.float())
+        val_losses.append(val_loss.item())
+        # Update tracking variables
+        preds = output.detach().cpu().numpy()
+        new_preds = np.zeros(preds.shape)
+        for i in range(len(preds)):
+            new_preds[i][np.argmax(preds[i])] = 1
+        val_accuracy = accuracy_score(y_true=batch['labels'].cpu().numpy().astype(int),
+                                  y_pred=new_preds.astype(int))
+        val_acc.append(val_accuracy)
 
     epoch_train_loss = np.mean(train_losses)
     epoch_val_loss = np.mean(val_losses)
@@ -237,22 +256,22 @@ for epoch in range(N_EPOCHS):
 
 # Test Set:
 test_losses = []
-test_acc = 0.0
+test_acc = []
 model.eval()
-for batch in test_loader:
-    outputs = model(input_ids=batch['input_ids'].to(device), attention_mask=batch['attention_mask'].to(device))
-    criterion = torch.nn.BCEWithLogitsLoss()
-    loss = criterion(outputs.view(-1, OUTPUT_DIM), batch['labels'].type_as(outputs).view(-1, OUTPUT_DIM))
-    test_losses.append(loss.item())
+test_h = model.init_hidden(BATCH_SIZE)
+for batch in valid_loader:
+    inputs, labels, attention_mask = batch['input_ids'].to(device), batch['labels'].to(device), batch['attention_mask'].to(device)
+    test_h = tuple([each.data for each in test_h])
+    output, test_h = model(inputs, test_h, attention_mask)
+    test_loss = criterion(output, labels.float())
+    test_losses.append(test_loss.item())
     # Update tracking variables
-    THRESHOLD = 0.5
-    preds = outputs.detach().cpu().numpy()
-    preds[preds >= THRESHOLD] = 1
-    preds[preds < THRESHOLD] = 0
-    # accuracy = accuracy_score(y_true=batch['labels'].detach().cpu().numpy().astype(int),
-    #                           y_pred=(np.rint(outputs.detach().cpu().numpy())).astype(int))
-    accuracy = accuracy_score(y_true=batch['labels'].detach().cpu().numpy().astype(int),
-                              y_pred=preds.astype(int))
-    test_acc += accuracy
+    preds = output.detach().cpu().numpy()
+    new_preds = np.zeros(preds.shape)
+    for i in range(len(preds)):
+        new_preds[i][np.argmax(preds[i])] = 1
+    test_accuracy = accuracy_score(y_true=batch['labels'].cpu().numpy().astype(int),
+                              y_pred=new_preds.astype(int))
+    test_acc.append(test_accuracy)
 test_acc_av = test_acc/len(test_loader.dataset)
 print(test_acc_av)
