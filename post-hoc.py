8d950fee4f216f25eed284e502a32a874b49a7d4
angeolson <78054238+angeolson@users.noreply.github.com>
2022-12-08 14:08:44 -0500

    Update post-hoc.py

--- post-hoc.py
+++ post-hoc.py
@@ -3,7 +3,16 @@
 # imports
 import pandas as pd
 import numpy as np 
+import argparse
+import os
 
+# set global vars
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--path", default=None, type=str, required=True)  # Path of file
+# args = parser.parse_args()
+# PATH = args.path
+PATH = '/Users/angelinaolson/Documents/Final-Project-Group-NLP/Data'
+os.chdir(PATH)
 
 # Helper Functions:
 def cleanLabel(row):
@@ -17,5 +26,12 @@ def cleanLabel(row):
     return label
 
 # read in test predictions
+test_df = pd.read_csv('test_predictions.csv')
 
+# get classes as dict 
+classes_dict = {0: 'negative', 1: 'neutral', 2: 'positive'}
+
+# clean labels
+test_df['target'] = test_df['target'].apply(cleanLabel)
+test_df['pred_label'] = test_df['pred_label'].apply(cleanLabel)
 
